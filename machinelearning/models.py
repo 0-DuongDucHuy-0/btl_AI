d084a3fe7384de81d85b77b03bf4f95e3f6c9c31
HuyHoangty <hoang9555266@gmail.com>
2023-11-14 15:50:46 +0700

    update project 6: train

--- machinelearning/models.py
+++ machinelearning/models.py
@@ -43,6 +43,14 @@ class PerceptronModel(object):
         Train the perceptron until convergence.
         """
         "*** YOUR CODE HERE ***"
+        shouldStop = False
+        while not shouldStop:
+            for x, y in dataset.iterate_once(batch_size=1):
+                if not self.get_prediction(x) == nn.as_scalar(y):
+                    self.w.update(x, nn.as_scalar(y))
+                    break
+            else:
+                shouldStop = True 
 
 class RegressionModel(object):
     """
